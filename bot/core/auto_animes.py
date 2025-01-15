1307d05a41ae1599f18c4af5f8a236ab3d6d37e3
Devashwinatx <ashwinrajaashwin7@gmail.com>
2025-01-15 08:30:12 +0530

    Update auto_animes.py

--- bot/core/auto_animes.py
+++ bot/core/auto_animes.py
@@ -1,18 +1,16 @@
 from asyncio import gather, create_task, sleep as asleep, Event
 from asyncio.subprocess import PIPE
-from os import path as ospath, system
+from os import path as ospath
 from aiofiles import open as aiopen
 from aiofiles.os import remove as aioremove
 from traceback import format_exc
 from base64 import urlsafe_b64encode
 from time import time
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram import filters
 
 from bot import bot, bot_loop, Var, ani_cache, ffQueue, ffLock, ff_queued
-from .tordownload import TorDownloader
 from .database import db
-from .func_utils import getfeed, encode, editMessage, sendMessage, convertBytes
+from .func_utils import encode, editMessage, sendMessage, convertBytes
 from .text_utils import TextEditor
 from .ffencoder import FFEncoder
 from .tguploader import TgUploader
@@ -25,106 +23,75 @@ btn_formatter = {
     '360': '𝟯𝟲𝟬𝗽'
 }
 
-@bot.on_message(filters.chat(Var.SOURCE_CHANNEL) & (filters.document | filters.video))
-async def handle_new_media(client, message):
-    if message.document:
-        name = message.document.file_name
-        file_id = message.document.file_id
-    elif message.video:
-        name = message.video.file_name if message.video.file_name else "Unnamed Video"
-        file_id = message.video.file_id
-
-    bot_loop.create_task(get_animes(name, file_id))
-                
-async def get_animes(name, file_id, force=False):
+async def process_telegram_file(file_id, chat_id):
     try:
-        aniInfo = TextEditor(name)
+        # Download file from Telegram
+        msg = await bot.get_messages(chat_id, file_id)
+        file_path = await bot.download_media(msg.document or msg.video)
+
+        # Start processing
+        aniInfo = TextEditor(file_path)
         await aniInfo.load_anilist()
-        ani_id, ep_no = aniInfo.adata.get('id'), aniInfo.pdata.get("episode_number")
-        if ani_id not in ani_cache['ongoing']:
-            ani_cache['ongoing'].add(ani_id)
-        elif not force:
-            return
-        if not force and ani_id in ani_cache['completed']:
-            return
-        if force or (not (ani_data := await db.getAnime(ani_id)) \
-            or (ani_data and not (qual_data := ani_data.get(ep_no))) \
-            or (ani_data and qual_data and not all(qual for qual in qual_data.values()))):
-            
-            if "[Batch]" in name:
-                await rep.report(f"File Skipped!\n\n{name}", "warning")
-                return
-            
-            await rep.report(f"New Anime File Found!\n\n{name}", "info")
-            post_msg = await bot.send_photo(
-                Var.MAIN_CHANNEL,
-                photo=await aniInfo.get_poster(),
-                caption=await aniInfo.get_caption()
-            )
-            
-            await asleep(1.5)
-            stat_msg = await sendMessage(Var.MAIN_CHANNEL, f"‣ <b>Anime Name :</b> <b><i>{name}</i></b>\n\n<i>Downloading...</i>")
-            # Download the file from Telegram
-            dl_path = f"./downloads/{name}"
-            await bot.download_media(file_id, file_name=dl_path)
-            if not ospath.exists(dl_path):
-                await rep.report(f"File Download Incomplete, Try Again", "error")
+        post_msg = await bot.send_photo(
+            Var.MAIN_CHANNEL,
+            photo=await aniInfo.get_poster(),
+            caption=await aniInfo.get_caption()
+        )
+        await asleep(1.5)
+        stat_msg = await sendMessage(
+            Var.MAIN_CHANNEL, f"‣ <b>File Name:</b> <b><i>{file_path}</i></b>\n\n<i>Queued to Encode...</i>"
+        )
+
+        # Queue and process file
+        post_id = post_msg.id
+        ffEvent = Event()
+        ff_queued[post_id] = ffEvent
+        if ffLock.locked():
+            await editMessage(stat_msg, f"‣ <b>File Name:</b> <b><i>{file_path}</i></b>\n\n<i>Queued to Encode...</i>")
+        await ffQueue.put(post_id)
+        await ffEvent.wait()
+
+        await ffLock.acquire()
+        btns = []
+        for qual in Var.QUALS:
+            filename = await aniInfo.get_upname(qual)
+            await editMessage(stat_msg, f"‣ <b>File Name:</b> <b><i>{filename}</i></b>\n\n<i>Encoding...</i>")
+            try:
+                out_path = await FFEncoder(stat_msg, file_path, filename, qual).start_encode()
+            except Exception as e:
+                await rep.report(f"Error: {e}, Retry!", "error")
                 await stat_msg.delete()
+                ffLock.release()
                 return
 
-            post_id = post_msg.id
-            ffEvent = Event()
-            ff_queued[post_id] = ffEvent
-            if ffLock.locked():
-                await editMessage(stat_msg, f"‣ <b>Anime Name :</b> <b><i>{name}</i></b>\n\n<i>Queued to Encode...</i>")
-                await rep.report("Added Task to Queue...", "info")
-            await ffQueue.put(post_id)
-            await ffEvent.wait()
-            
-            await ffLock.acquire()
-            btns = []
-            for qual in Var.QUALS:
-                filename = await aniInfo.get_upname(qual)
-                await editMessage(stat_msg, f"‣ <b>Anime Name :</b> <b><i>{name}</i></b>\n\n<i>Ready to Encode...</i>")
-                
-                await asleep(1.5)
-                await rep.report("Starting Encode...", "info")
-                try:
-                    out_path = await FFEncoder(stat_msg, dl_path, filename, qual).start_encode()
-                except Exception as e:
-                    await rep.report(f"Error: {e}, Cancelled, Retry Again!", "error")
-                    await stat_msg.delete()
-                    ffLock.release()
-                    return
-                await rep.report("Successfully Compressed. Now Going to Upload...", "info")
-                
-                await editMessage(stat_msg, f"‣ <b>Anime Name :</b> <b><i>{filename}</i></b>\n\n<i>Ready to Upload...</i>")
-                await asleep(1.5)
-                try:
-                    msg = await TgUploader(stat_msg).upload(out_path, qual)
-                except Exception as e:
-                    await rep.report(f"Error: {e}, Cancelled, Retry Again!", "error")
-                    await stat_msg.delete()
-                    ffLock.release()
-                    return
-                await rep.report("Successfully Uploaded File to Telegram...", "info")
-                
-                msg_id = msg.id
-                link = f"https://telegram.me/{(await bot.get_me()).username}?start={await encode('get-'+str(msg_id * abs(Var.FILE_STORE)))}"
-                
-                if post_msg:
-                    if len(btns) != 0 and len(btns[-1]) == 1:
-                        btns[-1].insert(1, InlineKeyboardButton(f"{btn_formatter[qual]} - {convertBytes(msg.document.file_size)}", url=link))
-                    else:
-                        btns.append([InlineKeyboardButton(f"{btn_formatter[qual]} - {convertBytes(msg.document.file_size)}", url=link)])
-                    await editMessage(post_msg, post_msg.caption.html if post_msg.caption else "", InlineKeyboardMarkup(btns))
-                    
-                await db.saveAnime(ani_id, ep_no, qual, post_id)
-                bot_loop.create_task(extra_utils(msg_id, out_path))
-            ffLock.release()
-            
-            await stat_msg.delete()
-            await aioremove(dl_path)
-        ani_cache['completed'].add(ani_id)
+            await editMessage(stat_msg, f"‣ <b>File Name:</b> <b><i>{filename}</i></b>\n\n<i>Uploading...</i>")
+            try:
+                msg = await TgUploader(stat_msg).upload(out_path, qual)
+            except Exception as e:
+                await rep.report(f"Error: {e}, Retry!", "error")
+                await stat_msg.delete()
+                ffLock.release()
+                return
+
+            msg_id = msg.id
+            link = f"https://telegram.me/{(await bot.get_me()).username}?start={await encode('get-'+str(msg_id * abs(Var.FILE_STORE)))}"
+            if post_msg:
+                if len(btns) != 0 and len(btns[-1]) == 1:
+                    btns[-1].insert(1, InlineKeyboardButton(f"{btn_formatter[qual]} - {convertBytes(msg.document.file_size)}", url=link))
+                else:
+                    btns.append([InlineKeyboardButton(f"{btn_formatter[qual]} - {convertBytes(msg.document.file_size)}", url=link)])
+                await editMessage(post_msg, post_msg.caption.html if post_msg.caption else "", InlineKeyboardMarkup(btns))
+
+        ffLock.release()
+        await stat_msg.delete()
+        await aioremove(file_path)
     except Exception as error:
         await rep.report(format_exc(), "error")
+
+async def fetch_animes():
+    await rep.report("Fetch Animes Started !!", "info")
+    while True:
+        await asleep(60)
+        if ani_cache['fetch_animes']:
+            for task in ani_cache['tasks']:
+                await process_telegram_file(task['file_id'], task['chat_id'])
